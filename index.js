e72a616e4f85dfbb2118d71e0041213e4d5eeab2
jojogames <joshuachemin@gmail.com>
2019-02-27 16:07:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bot.on('ready', function() {
     console.log("Connectedç");
 });
 
-bot.login(TOKEN);
+bot.login("NTUwMjkzMDc0MDYwNzcxMzI5.D1gWzQ.atoSuFNsjg79id97C8skWBDbow8");
 
 
 bot.on('message', message => {
@@ -24,33 +24,31 @@ bot.on('message', message => {
         message.reply("Pong");
         console.log("Commande Ping effectué");
     }
-    if (message.content.startsWith('*play')) {
-        // On récupère le premier channel audio du serveur
-        let voiceChannel = message.guild.channels
-          .filter(function (channel) { return channel.type === 'voice' })
-          .first()
-        // On récupère les arguments de la commande 
-        // il faudrait utiliser une expression régulière pour valider le lien youtube
-        let args = message.content.split(' ')
-        // On rejoint le channel audio
-        voiceChannel
-          .join()
-          .then(function (connection) {
-            // On démarre un stream à partir de la vidéo youtube
-            let stream = YoutubeStream(args[1])
-            stream.on('error', function () {
-              message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-              connection.disconnect()
-            })
-            // On envoie le stream au channel audio
-            // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
-            connection
-              .playStream(stream)
-              .on('end', function () {
-                connection.disconnect()
-              })
+  if (message.content.startsWith('!play')) {
+    // On récupère le premier channel audio du serveur
+    let voiceChannel = message.guild.channels
+      .filter(function (channel) { return channel.type === 'voice' })
+      .first()
+    // On récupère les arguments de la commande 
+    // il faudrait utiliser une expression régulière pour valider le lien youtube
+    let args = message.content.split(' ')
+    // On rejoint le channel audio
+    voiceChannel
+      .join()
+      .then(function (connection) {
+        // On démarre un stream à partir de la vidéo youtube
+        let stream = YoutubeStream(args[1])
+        stream.on('error', function () {
+          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+          connection.disconnect()
+        })
+        // On envoie le stream au channel audio
+        // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
+        connection
+          .playStream(stream)
+          .on('end', function () {
+            connection.disconnect()
           })
-      }
-    
-    })
+      })
+  }
 });
